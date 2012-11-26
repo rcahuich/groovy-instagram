2a6060a3c588d39be003ab9f808d8f23e2d10af2
Jeff Thorne <jeff.thorne@gmail.com>
2012-11-26 07:26:37 -0800

    added configuration class.

---
+++ src/groovy/instagram/Configuration.groovy
@@ -0,0 +1,8 @@
+package groovy.instagram
+
+class Configuration {
+	
+    static String DEFAULT_ENDPOINT = "https://api.instagram.com/v1/"
+    static String CLIENT_ID = ""
+    static String CLIENT_SECRET = ""
+}
